7168beb8beef6e7d4a00d0528d79413fb82abcd3
Lixo Ten <52259786+lixoten@users.noreply.github.com>
2024-03-02 02:18:42 -0800

    Stage 3 Done

--- Coffee Machine (JavaScript)/task/main.js
+++ Coffee Machine (JavaScript)/task/main.js
@@ -1,67 +1,42 @@
-// Stage 2
+// Stage 3
 // Use "input()" to input a line from the user
 // Use "input(str)" to print some text before requesting input
 // You will need this in the following stages
 const input = require('sync-input');
 
-console.log("Write how many cups of coffee you will need:");
+const coffeeRecipe = {
+    water : 200,
+    milk :  50,
+    beans : 15
+};
 
-// Note: Type/Numeric conversion
+const coffeeMachine = {
+    water : 0,
+    milk :  0,
+    beans : 0
+};
+
+console.log("Write how many ml of water the coffee machine has:");
+coffeeMachine["water"] = Number(input());
+
+console.log("Write how many ml of milk the coffee machine has:");
+coffeeMachine["milk"] = Number(input());
+
+console.log("Write how many grams of coffee beans the coffee machine has:");
+coffeeMachine["beans"]= Number(input());
+
+console.log("Write how many cups of coffee you will need:");
 let cupsNeeded = Number(input());
 
-// Based on Lessons to this point
-let waterNeeded = 200;
-let milkNeeded = 50;
-let beansNeeded = 15;
+let result =
+    Math.floor(Math.min(coffeeMachine["water"] / coffeeRecipe["water"],
+        coffeeMachine["milk"] / coffeeRecipe["milk"],
+        coffeeMachine["beans"] / coffeeRecipe["beans"]));
 
-let totalWaterNeed = waterNeeded * cupsNeeded;
-let totalMilkNeed  = milkNeeded  * cupsNeeded;
-let totalCoffeeNeed= beansNeeded  * cupsNeeded;
-
-console.log(`For ${cupsNeeded} cups of coffee you will need:`);
-console.log(totalWaterNeed + ` ml of water`);
-console.log(totalMilkNeed + ` ml of milk`);
-console.log(totalCoffeeNeed + ` g of beans`);
-
-// Simple Arrays
-// let item = ["water", "milk", "beans"];
-// let amt = [200, 50, 15];
-// let unit = ["ml", "ml", "g"];
-//
-// let waterNeed = amt[0] * cupsNeeded;
-// let milkNeed  = amt[1]  * cupsNeeded;
-// let coffeeNeed= amt[2]  * cupsNeeded;
-//
-// console.log(`For ${cupsNeeded} cups of coffee you will need:`)
-// console.log(waterNeed + ` ${unit[0]} of ${item[0]}`);
-// console.log(milkNeed + ` ${unit[1]} of ${item[1]}`);
-// console.log(coffeeNeed + ` ${unit[2]} of ${item[2]}`);
-
-// more advanced with associative array///////////////
-// const  ingredientArr  = {
-//     water :  {
-//         name: "water",
-//         amount: 200,
-//         unit: "ml",
-//     },
-//     milk :  {
-//         name: "milk",
-//         amount: 50,
-//         unit: "ml",
-//     },
-//     coffee :  {
-//         name: "coffee beans",
-//         amount: 15,
-//         unit: "g",
-//     }
-// };
-
-// let waterNeed = ingredientArr["water"]["amount"] * cupsNeeded;
-// let milkNeed  = ingredientArr["milk"]["amount"] * cupsNeeded;
-// let coffeeNeed= ingredientArr["coffee"]["amount"] * cupsNeeded;
-//
-// console.log(`For ${cupsNeeded} cups of coffee you will need:`)
-// console.log(waterNeed + ` ${ingredientArr["water"]["unit"]} of ${ingredientArr["water"]["name"]}`);
-// console.log(milkNeed + ` ${ingredientArr["milk"]["unit"]} of ${ingredientArr["milk"]["name"]}`);
-// console.log(coffeeNeed + ` ${ingredientArr["coffee"]["unit"]} of ${ingredientArr["coffee"]["name"]}`);
-//
+if (result < cupsNeeded) {
+    console.log(`No, I can make only ${result} cups of coffee`);
+} else if (result === cupsNeeded) {
+    console.log("Yes, I can make that amount of coffee");
+} else {
+    console.log(`Yes, I can make that amount of coffee (and even ${result-1} more than that)`);
+}
